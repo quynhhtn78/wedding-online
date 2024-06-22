8b05d50fa0d7d2457deefc1ba1a6044043fb0b26
Quynh Hoang <74813910+htnquynh@users.noreply.github.com>
2024-06-23 03:24:51 +0700

    fix dashboard view

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,6 @@ import DashboardView from '../views/DashboardView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  mode: history,
   routes: [
     {
       path: '/',
